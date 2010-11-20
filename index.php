eb3f3e37c92635f25ade7b92b1a579e6b968dc26
Piete Sartain <pesartain@googlemail.com>
2010-11-20 23:16:37 +0000

    More advanced prototype. Added removal of units from the drop area. Added most of the info into the main unit list.

--- index.php
+++ index.php
@@ -2,29 +2,103 @@
 
 // Figure list as an array
 
-$fid = fopen("figures.txt");
-$figures = fgetcsv($fid);
+$fid = fopen("figures.txt",'r');
+$headings = fgetcsv($fid);
+//print_r($figures);
 
-unset($figures[0]);
+//unset($figures[0]);
+
+//function parse_ability($name) {
+//	$retval = str_replace(" ","-",strtolower($name));
+
+function createAbility($ability,$cost,$type) {
+	return "<td 	class='ability' 
+					style=\"background-image: 
+							url('res/special_abilities/".$type."-".str_replace(" ","-",strtolower($ability)).".png')
+						\"
+					>&nbsp;</td><td>".$cost."</td>";
+}
 
 function createLi($figure) {
-	$str = "<li id=''>";
+	$str = "<li id='".$figure[0]."'>";
 	
-	//$str = "";
+	$base = strtolower($figure[26]).$figure[27];
 	
-	print_r($figure);
+	// 26: colour; 27: type;
+	$str .= "<div style='float: left; width:25px;' class='base'><img src='res/figures/$base.png' style='width:25px;' /></div>";
+
+	switch (strtoupper(substr($figure[0],1,1))) {
+	case "E":
+		$faction = "egyptian";
+		break;
+	case "R":
+		$faction = "roman";
+		break;
+	case "H":
+		$faction = "han";
+		break;
+	case "M":
+		$faction = "mercenary";
+		break;
+	}
+
+	$str .= "<div style='float: left; width: 80%;'>";
+		$str .= "<div class='$faction base_id'>".$figure[0]."</div>";
+		$str .= "<div class='name'>		".$figure[1]."	</div>";
+		$str .= "<br /><table><tr>";
+		$str .= "<td class='red'>&nbsp;</td><td>	".$figure[2]."	</td>";
+		$str .= "<td class='blue'>&nbsp;</td><td>	".$figure[3]."	</td>";
+		$str .= "<td class='white'>&nbsp;</td><td>	".$figure[4]."	</td>";
+		$str .= "<td class='move'>&nbsp;</td><td>	".$figure[5]."	</td>";
+		$str .= "</tr><tr>";
+
+		if ($figure[6] != "") {
+			$str .= createAbility($figure[6],$figure[7],"lsa");
+		} else {
+			$str .= "<td>&nbsp;</td><td>&nbsp;</td>";
+		}
+	
+		if ($figure[8] != "") {
+			$str .= createAbility($figure[8],$figure[9],"lsa");
+		} else {
+			$str .= "<td>&nbsp;</td><td>&nbsp;</td>";
+		}
+	
+		if ($figure[10] != "") {
+			$str .= createAbility($figure[10],$figure[11],"gsa");
+		} else {
+			$str .= "<td>&nbsp;</td><td>&nbsp;</td>";
+		}
+	
+		if ($figure[12] != "") {
+			$str .= createAbility($figure[12],$figure[13],"gsa");
+		} else {
+			$str .= "<td>&nbsp;</td><td>&nbsp;</td>";
+		}
+
+		$str .= "</tr></table>";
+	$str .= "</div>";
+	
+	$str .= "<div style='float: left;'>";
+	$str .= "</div>";
+	
+
 	
 	$str .= "</li>";
 	return $str;
 }
 
-function createUl($figures) {
+function createUl($fid) {
 	$str = "<ul id='figure_list'>";
 
-	foreach ($figure as $figures) {
+	/*foreach ($figures as $figure) {
+		$str .= createLi($figure);
+	}*/
+	
+	while(($figure = fgetcsv($fid)) !== FALSE) {
 		$str .= createLi($figure);
 	}
-	
+
 	$str .= "</ul>";
 	
 	return $str;
@@ -45,24 +119,59 @@ function createUl($figures) {
 
 	$(function() {
 
+		$('select[name="faction"]').change(function() {
+			var fimg = "res/insignia/"+$('select[name="faction"]').val()+".png";
+			$("header img").attr('src',fimg);
+		});
+
 		$( "#figure_list>li" ).draggable({
 			revert: true, 
 			helper: "clone",
 		});
 
 		$("#drop_area").droppable({
-			accept: "li",
+			accept: "li, div",
 
 			drop: function(ev, ui) {
 
-				var id = $(ui.draggable).attr('id')+":visible";
+				if ($(ui.draggable).is("li")) {
+	
+					var id = $(ui.draggable).attr('id')+":visible";
+					
+					var red = $("#"+id+" .red").text();
+					var white = $("#"+id+" .white").text();
+					var blue = $("#"+id+" .blue").text();
+					var move = $("#"+id+" .move").text();
+					
+					var base_id = $("#"+id+" .base_id").text();
+					//var name = $("#"+id+" .name").text();
+					
+					var base = $("#"+id+" img").attr('src');
+	
+					//console.log(base);
+	
+					$("<div class='base'><img src='"+base+"' /><span>"+base_id+"</span></div>").appendTo(this).draggable({
+						//containment: "parent", // Constrains to the drop area
+						grid: [10, 10],
+						//revert: "invalid", // Reverts the draggable when you drop on an invalid area
+						start: function(event, ui) {
+							// flag to indicate that we want to remove element on drag stop
+							ui.helper.removeMe = true;
+						},
+						stop: function(event, ui) {
+							// remove draggable if flag is still true
+							// which means it wasn't unset on drop into parent
+							// so dragging stopped outside of parent
+							if (ui.helper.removeMe) {
+							  ui.helper.remove();
+							}
+						}
+					});
+				} else {
 				
-				var points = $("#"+id+" > .points").text();
-
-				$("<div id='stuff'>"+points+"</div>").appendTo(this).draggable({
-					containment: "parent", 
-					grid: [20, 20],
-				});
+					ui.helper.removeMe = false;
+				
+				}
 				
 			}
 
@@ -73,17 +182,80 @@ function createUl($figures) {
 
 <style>
 
+body {font-family: tahoma;}
+
 div#drop_area {width: 800px; height: 600px; border: 1px solid black; float: left;}
 
-ul#figure_list {float: left;}
+div#drop_area > div > span {position: relative; left: 10px; top: -12px; background: #ffffdd; font-size: 70%;}
 
-ul#figure_list li {border: 1px solid blue; width: 200px; background: blue;}
+ul#figure_list {padding: 0px; margin: 0px;}
+ul#figure_list li {	border: 1px solid black; width: 380px; background: silver; 
+					list-style: none; padding: 0px; margin: 0px;
+				}
 
-#draggable { width: 150px; height: 150px; padding: 0.5em; background: blue;}
+ul#figure_list li:after, 
+header:after 
+{
+	content: ".";
+	display: block;
+	height: 0;
+	clear: both;
+	visibility: hidden;
+}
 
-.demo { width: 800px; height: 600px; }
+ul#figure_list > li > p {margin: 0px; padding: 0px; display: block;}
 
-div#drop_area > div { background: yellow; border: 0px solid red; width: 100px; height: 100px; position: absolute; float: left;}
+ul#figure_list > li table {width: 100%;}
+ul#figure_list > li table td {width: 12.5%;}
+
+.red, .white, .blue, .move {
+							/*
+							display: inline; 
+							width: 120px; height: 25px;
+							padding-left: 40px;
+							
+							width: 40px;
+							*/
+							background-repeat:no-repeat;
+							background-position: 0px -10px;
+							opacity: 0.1;
+							}
+
+.red 	{	background-image: url('res/attributes/red.png');	}
+.white 	{	background-image: url('res/attributes/white.png');	}
+.blue 	{	background-image: url('res/attributes/blue.png');	}
+.move 	{	background-image: url('res/attributes/move.png');	}
+
+.ability{		/*
+				display: inline; width: 110px; height: 20px;
+				padding-left: 50px; color: #000;
+				
+				width: 25%;
+				*/
+				background-repeat:no-repeat;
+				background-position: 0px -15px;
+				opacity: 0.2;
+		}
+
+.base_id{	display: inline; width: 110px; height: 20px;
+				background-repeat:no-repeat;
+				background-position: 0px -30px;
+				padding-left: 40px; color: #ffffdd; font-weight: bold;
+		}
+.mercenary 	{	background-image: url('res/insignia/mercenary.png');	}
+.han 		{	background-image: url('res/insignia/han.png');	}
+.roman 		{	background-image: url('res/insignia/roman.png');	}
+.egyptian 	{	background-image: url('res/insignia/egyptian.png');	}
+
+.base {width: 40px; height: 60px;}
+
+.name {width: 290px; display: inline; margin-left: 10px; font-weight: bold; font-size: 120%;}
+
+div#drop_area > div {/* background: yellow; border: 0px solid red; */
+					/*width: 100px; height: 100px; */
+					position: absolute; float: left;}
+
+div#scroll_list {overflow: auto; height: 600px; width: 400px; float: left;}
 
 </style>
 
@@ -91,13 +263,21 @@ div#drop_area > div { background: yellow; border: 0px solid red; width: 100px; h
 
 <body>
 
-<div>
-	<ul id="figure_list">
-		<li id='fid1'>Item1 <p class='points'>100</p></li>
-		<li id='fid2'>Item2</li>
-		<li id='fid3'>Item3</li>
-		<li id='fid4'>Item4</li>
-	</ul>
+<header>
+
+<img src="res/insignia/egyptian.png" />
+
+<select name='faction'>
+	<option value="egyptian">Egypt</option>
+	<option value="han">Han Dynasty</option>
+	<option value="mercenary">Mercenary</option>
+	<option value="roman">Rome</option>
+</select>
+
+</header>
+
+<div id="scroll_list">
+<?php echo createUl($fid) ?>
 </div>
 
 <div id="drop_area">
